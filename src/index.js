ddec507afdaa310fb29792aea6eafc15d5a853a5
AnnaDudnyk <anna7100156@gmail.com>
2022-09-04 13:47:17 +0300

    API Integration for the weather forecast

--- src/index.js
+++ src/index.js
@@ -1,51 +1,34 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// days array for getting week day based on Date.getDay()
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+// set api key here and use it in all places
+let apiKey = "d296f3b5eb9c5ad34d0650aec94e4ce3";
 
 function formatDate(date) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   let day = days[date.getDay()];
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+
   let month = months[date.getMonth()];
 
   return `${date.getHours()}:${date.getMinutes()} - ${day}, ${date.getDate()} ${month}`;
@@ -56,6 +39,7 @@ function titleCase(name) {
 }
 
 let timeSelector = document.querySelector("#current-time");
+let forecastContainer = document.querySelector("#forecast-container");
 
 let currentTime = new Date();
 
@@ -66,7 +50,74 @@ let currentCity = document.querySelector("#current-city");
 let cityInput = document.querySelector("#city-input");
 let searchForm = document.querySelector("#search-form");
 
+// forecast function to make request with coordinates tp api and get forecast data
+function processForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  // making request to get forecast data
+  axios.get(apiUrl).then((response) => {
+    let forecasts = [];
+
+    // remove first element from array because we dont need current date to display
+    let daylyData = response.data.daily.slice(1);
+
+    for (let i = 0; i < daylyData.length; i++) {
+      let forecast = daylyData[i];
+
+      let forecastDate = new Date(forecast.dt * 1000);
+
+      let forecastDayName = days[forecastDate.getDay()];
+
+      let minTemp = Math.round(forecast.temp.min);
+      let maxTemp = Math.round(forecast.temp.max);
+
+      let weatherIcon = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+
+      forecasts.push({
+        weekday: forecastDayName,
+        date: forecastDate,
+        minMax: `${minTemp}°/${maxTemp}°`,
+        icon: weatherIcon,
+      });
+    }
+
+    if (forecasts.length > 0) {
+      let forecastInterval = document.querySelector("#forecast-interval");
+      let forecastItems = document.querySelector("#forecasts-items");
+
+      let forecastHtml = "";
+
+      for (let i = 0; i < forecasts.length; i++) {
+        let item = forecasts[i];
+        forecastHtml += `<li class="card-background">
+                                <span>${item.weekday}</span>
+                                <span>${item.minMax}</span>
+                                <span><img
+                                    src="${item.icon}"
+                                    alt="Weather icon"
+                                    id="icon"
+                                /></span>
+                            </li>`;
+      }
+
+      forecastItems.innerHTML = forecastHtml;
+
+      let firstDay = forecasts[0].date;
+      let lastDay = forecasts[forecasts.length - 1].date;
+
+      forecastInterval.innerHTML = `${firstDay.getDate()} ${
+        months[firstDay.getMonth()]
+      } - ${lastDay.getDate()} ${months[lastDay.getMonth()]}`;
+    }
+
+    forecastContainer.classList.add("is-active");
+  });
+}
+
 function showTemp(response) {
+  if (cityInput && cityInput.value) {
+    currentCity.innerHTML = titleCase(cityInput.value);
+  }
   let temp = Math.round(response.data.main.temp);
   let currentDegree = document.querySelector("#current-degree");
   currentDegree.innerHTML = `${temp}°`;
@@ -79,17 +130,22 @@ function showTemp(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  processForecast(response.data.coord);
+}
+
+function processWeather(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
 }
 
 searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
   if (cityInput && cityInput.value) {
-    currentCity.innerHTML = titleCase(cityInput.value);
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric`;
-
-    let apiKey = "d296f3b5eb9c5ad34d0650aec94e4ce3";
-
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
+    processWeather(cityInput.value);
   }
+});
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  processWeather("Kyiv");
 });
